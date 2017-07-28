ba4e6820c2f3fa9a3b61191d854d95f9222e2a15
elel123 <ho.elton@gmail.com>
2017-07-28 09:08:59 -0700

    Code for Milestone 2 using LED strips

--- NightLightCode(Milestone2).ino
+++ NightLightCode(Milestone2).ino
@@ -191,24 +191,6 @@ void black(uint8_t wait) {
   delay(20);
 }
 
-void fadeInAndOut(uint8_t red, uint8_t green, uint8_t blue, uint8_t wait) {
-
-  for (uint8_t b = 0; b < 255; b++) {
-    for (uint8_t i = 0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, red * b / 255, green * b / 255, blue * b / 255);
-    }
-    strip.show();
-    delay(wait);
-  }
-
-  for (uint8_t b = 255; b > 0; b--) {
-    for (uint8_t i = 0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, red * b / 255, green * b / 255, blue * b / 255);
-    }
-    strip.show();
-    delay(wait);
-  }
-}
 // Slightly different, this makes the rainbow equally distributed throughout
 void rainbowCycle(uint8_t wait) {
   uint16_t i, j;
